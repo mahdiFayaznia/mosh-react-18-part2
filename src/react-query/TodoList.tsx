4226f37731a4347aa1a78e0f4b25a532d7ed9684
co.fayaznia <mahdifayaznia@gmail.com>
2024-01-30 11:45:41 +0330

    Fetching Data

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
@@ -9,21 +10,38 @@ interface Todo {
 }
 
 const TodoList = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
+  const fetchTodos = () =>
     axios
-      .get("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => setTodos(res.data))
-      .catch((error) => setError(error));
-  }, []);
+      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.data);
 
-  if (error) return <p>{error}</p>;
+  // const query = useQuery({
+  // const { data } = useQuery({
+  const { data: todos } = useQuery({
+    queryKey: ["todos"],
+    // queryFn: () =>
+    //   axios
+    //     .get("https://jsonplaceholder.typicode.com/todos")
+    //     .then((res) => res.data),
+    queryFn: fetchTodos,
+  });
+
+  // const [todos, setTodos] = useState<Todo[]>([]);
+  // const [error, setError] = useState("");
+
+  // useEffect(() => {
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/todos")
+  //     .then((res) => setTodos(res.data))
+  //     .catch((error) => setError(error));
+  // }, []);
+
+  // if (error) return <p>{error}</p>;
 
   return (
     <ul className="list-group">
-      {todos.map((todo) => (
+      {/* {data?.map((todo) => ( */}
+      {todos?.map((todo) => (
         <li key={todo.id} className="list-group-item">
           {todo.title}
         </li>
