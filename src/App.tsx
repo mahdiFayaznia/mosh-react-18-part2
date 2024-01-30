b07049be183e0a0b6555303553c6909a4aaf13bc
Mahdi Fayaznia <mahdifayaznia@gmail.com>
2024-01-30 22:42:32 +0330

    Fetching Posts Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import "./App.css";
+import PostList from "./react-query/PostList";
 import TodoList from "./react-query/TodoList";
 
 function App() {
   // return <h1>React Starter Project</h1>;
-  return <TodoList />;
+  return (
+    <>
+      <h1 className="my-5">TodoList</h1>
+      <TodoList />
+
+      <hr className="my-5" />
+
+      <h1 className="my-5">PostList</h1>
+      <PostList />
+    </>
+  );
 }
 
 export default App;
--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,31 +1,38 @@
-import axios from "axios";
+// import axios from "axios";
 import { useEffect, useState } from "react";
+import usePosts from "./hooks/usePosts";
 
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-  userId: number;
-}
+// interface Post {
+//   id: number;
+//   title: string;
+//   body: string;
+//   userId: number;
+// }
 
 const PostList = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [error, setError] = useState("");
+  // const [posts, setPosts] = useState<Post[]>([]);
+  // const [error, setError] = useState("");
 
-  useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => setPosts(res.data))
-      .catch((error) => setError(error));
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/posts")
+  //     .then((res) => setPosts(res.data))
+  //     .catch((error) => setError(error));
+  // }, []);
 
-  if (error) return <p>{error}</p>;
+  // if (error) return <p>{error}</p>;
+
+  const { data: posts, error, isLoading } = usePosts();
+
+  if (error) return <p>{error.message}</p>;
+
+  if (isLoading) return <p>Loading...</p>;
 
   return (
-    <ul className="list-group">
-      {posts.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
+    <ul className="menu bg-base-200 w-full rounded-box">
+      {posts?.map((post) => (
+        <li key={post.id}>
+          <a>{post.title}</a>
         </li>
       ))}
     </ul>
