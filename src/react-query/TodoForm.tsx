2f02c1609eda7c839181f83203c61f46a43436cf
Mahdi Fayaznia <mahdifayaznia@gmail.com>
2024-02-09 20:56:47 +0330

    Handling Mutation Errors

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -6,12 +6,14 @@ import axios from "axios";
 const TodoForm = () => {
   const queryClient = useQueryClient(); // access to queryClient
 
-  const addTodo = useMutation({
+  // useMutation: <data that get from back-end, error, data that send to back-end>
+  const addTodo = useMutation<Todo, Error, Todo>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
 
+    // onSuccess: (object that get from back-end, object that send to back-end)
     onSuccess: (savedTodo, newTodo) => {
       console.log("savedTodo", savedTodo);
 
@@ -31,28 +33,37 @@ const TodoForm = () => {
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <form
-      className="flex space-x-5 mb-5"
-      onSubmit={(event) => {
-        event.preventDefault();
+    <>
+      {addTodo.error && (
+        <div className="toast">
+          <div className="alert alert-error">
+            <span>{addTodo.error.message}</span>
+          </div>
+        </div>
+      )}
+      <form
+        className="flex space-x-5 mb-5"
+        onSubmit={(event) => {
+          event.preventDefault();
 
-        // check the existing of value in input
-        if (ref.current && ref.current.value)
-          addTodo.mutate({
-            id: 0,
-            title: ref.current?.value,
-            completed: false,
-            userId: 1,
-          });
-      }}
-    >
-      <input
-        ref={ref}
-        type="text"
-        className="input input-bordered w-full max-w-md"
-      />
-      <button className="btn btn-primary">Add</button>
-    </form>
+          // check the existing of value in input
+          if (ref.current && ref.current.value)
+            addTodo.mutate({
+              id: 0,
+              title: ref.current?.value,
+              completed: false,
+              userId: 1,
+            });
+        }}
+      >
+        <input
+          ref={ref}
+          type="text"
+          className="input input-bordered w-full max-w-md"
+        />
+        <button className="btn btn-primary">Add</button>
+      </form>
+    </>
   );
 };
 
