7edf6a98a242e3d512a457c3ab3cded0d70854c1
Mahdi Fayaznia <mahdifayaznia@gmail.com>
2024-03-22 15:56:37 +0330

    Creating Complex Actions

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 import Counter from "./state-management/Counter";
+import TaskList from "./state-management/TaskList";
 
 function App() {
   // return <h1>React Starter Project</h1>;
@@ -15,10 +16,13 @@ function App() {
       <hr className="my-5" />
 
       <h1 className="my-5 text-4xl">PostList</h1>
-    <PostList /> */}
+      <PostList /> */}
 
-      <h1 className="my-5 text-4xl">Counter</h1>
-      <Counter />
+      {/* <h1 className="my-5 text-4xl">Counter</h1>
+      <Counter /> */}
+
+      <h1 className="my-5 text-4xl">TaskList</h1>
+      <TaskList />
     </>
   );
 }
--- src/state-management/TaskList.tsx
+++ src/state-management/TaskList.tsx
@@ -1,36 +1,42 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+import tasksReducer from "./reducers/tasksReducer";
 
-interface Task {
-  id: number;
-  title: string;
-}
+// interface Task {
+//   id: number;
+//   title: string;
+// }
 
 const TaskList = () => {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, dispatch] = useReducer(tasksReducer, []);
 
   return (
     <>
       <button
         onClick={() =>
-          setTasks([{ id: Date.now(), title: "Task " + Date.now() }, ...tasks])
+          // setTasks([{ id: Date.now(), title: "Task " + Date.now() }, ...tasks])
+          dispatch({
+            type: "ADD",
+            task: { id: Date.now(), title: "Task " + Date.now() },
+          })
         }
         className="btn btn-primary my-3"
       >
         Add Task
       </button>
-      <ul className="list-group">
+      <ul className="menu bg-base-200 w-full rounded-box">
         {tasks.map((task) => (
-          <li
-            key={task.id}
-            className="list-group-item d-flex justify-content-between align-items-center"
-          >
-            <span className="flex-grow-1">{task.title}</span>
-            <button
-              className="btn btn-outline-danger"
-              onClick={() => setTasks(tasks.filter((t) => t.id !== task.id))}
-            >
-              Delete
-            </button>
+          <li key={task.id}>
+            <a className="w-full flex justify-between">
+              <span>{task.title}</span>
+              <button
+                className="btn btn-error w-20"
+                // onClick={() => setTasks(tasks.filter((t) => t.id !== task.id))}
+                onClick={() => dispatch({ type: "DELETE", taskId: task.id })}
+              >
+                Delete
+              </button>
+            </a>
           </li>
         ))}
       </ul>
