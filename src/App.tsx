73373ad9929e5c5554bc5aa91ff1ff63234c59fa
Mahdi Fayaznia <mahdifayaznia@gmail.com>
2024-03-22 15:27:25 +0330

    Consolidating State Logic with a Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import "./App.css";
 import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
+import Counter from "./state-management/Counter";
 
 function App() {
   // return <h1>React Starter Project</h1>;
   return (
     <>
-      <h1 className="my-5 text-4xl">TodoList</h1>
+      {/* <h1 className="my-5 text-4xl">TodoList</h1>
       <TodoForm />
       <TodoList />
 
       <hr className="my-5" />
 
       <h1 className="my-5 text-4xl">PostList</h1>
-      <PostList />
+    <PostList /> */}
+
+      <h1 className="my-5 text-4xl">Counter</h1>
+      <Counter />
     </>
   );
 }
---
+++ src/state-management/reducers/counterReducer.ts
@@ -0,0 +1,14 @@
+// reducer: currentState, action -> return newState
+
+interface Action {
+  // type: string;
+  type: "INCREMENT" | "RESET";
+}
+
+const counterReducer = (state: number, action: Action): number => {
+  if (action.type === "INCREMENT") return state + 1;
+  if (action.type === "RESET") return 0;
+  return state;
+};
+
+export default counterReducer;
