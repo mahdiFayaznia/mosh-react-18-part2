5542e0e961ef9c43d93661b9c510b60430371366
Mahdi Fayaznia <mahdifayaznia@gmail.com>
2024-03-22 17:54:05 +0330

    Creating a Hook to Access Context

--- src/state-management/LoginStatus.tsx
+++ src/state-management/LoginStatus.tsx
@@ -1,12 +1,14 @@
 import { useContext, useReducer, useState } from "react";
 import authReducer from "./reducers/authReducer";
 import AuthContext from "./contexts/authContext";
+import useAuth from "./hooks/useAuth";
 
 const LoginStatus = () => {
   // const [user, setUser] = useState("");
   // const [user, dispatch] = useReducer(authReducer, "");
 
-  const { user, dispatch } = useContext(AuthContext);
+  // const { user, dispatch } = useContext(AuthContext);
+  const { user, dispatch } = useAuth();
 
   if (user)
     return (
