a256158c8618ec6282750be4bb64afada4565692
Mahdi Fayaznia <mahdifayaznia@gmail.com>
2024-07-15 22:49:34 +0330

    Handling Errors

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -5,11 +5,13 @@ import ContactPage from "./ContactPage";
 import UserDetail from "./UserDetail";
 import Layout from "./Layout";
 import UsersPage from "./UsersPage";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />, // just handle error in root route
     children: [
       // { path: "", element: <HomePage /> },
       { index: true, element: <HomePage /> },
